fe5b9f96ffae86dcbf8f27c4a5ffa60880011d27
Eugene Paitoo <eugenepaitoo@gmail.com>
2021-07-24 21:37:30 +0000

    show user info on homepage

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class HomeController {
 
+    private UserService userService;
+
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping()
-    public String getHomePage() {
+    public String getHomePage(Model model, Authentication authentication) {
+        String username = authentication.getName();
+        User user = this.userService.getUser(username);
+        String fullName = user.getFirstName() + " " + user.getLastName();
+
+        model.addAttribute("userFullName", fullName);
+        model.addAttribute("username", username);
+
         return "home";
     }
 }
